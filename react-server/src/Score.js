4fe646adbc332fdba4868f3ad6dc1b920a32df1a
GrinJessie <lijing@ualberta.ca>
2018-03-13 21:57:45 +0000

    implement addr bar autocomplete from google api, change redirect state to pass more info

--- react-server/src/Score.js
+++ react-server/src/Score.js
@@ -7,8 +7,10 @@ class Score extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: this.props.location.state ? this.props.location.state.data.location : null,
-      radius: this.props.location.state ? this.props.location.state.data.radius : null
+      address: this.props.location.state ? this.props.location.state.data.location.address : null,
+      longitude: this.props.location.state ? this.props.location.state.data.location.longitude : null,
+      latitude: this.props.location.state ? this.props.location.state.data.location.latitude : null,
+      radius: this.props.location.state ? this.props.location.state.data.radius : '800'
 
     };
   }
@@ -25,7 +27,7 @@ class Score extends Component {
           <h1 className="display-4">Hello, find-score page!</h1>
           <hr className="my-4" />
             <p className="lead">
-              <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+              <a className="btn btn-primary btn-lg" role="button">Learn more</a>
             </p>
         </div>
       </div>
