f6c12ce279f9c0f8c5a79bb32b7fb85f207e4e67
Viccy Grace <viccy.c.grace@gmail.com>
2018-03-13 11:29:52 -0600

    project seed changes

--- db/seeds/projects_seed.js
+++ db/seeds/projects_seed.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const seeder  = require('knex-csv-seeder');
-// import seeder from 'knex-csv-seeder';
-
-exports.seed = seeder.default({
-  table: 'projects',
-  file: 'db/csv_files/projects.csv',
-  // recordsPerQuery: 100,
-  // encoding: 'utf8' default encoding
-  parser: {
-    delimiter: '\n',
-  //   quote: '"',
-  // //   escape: '\\'
-  }
-});
-=======
 const knexConfig  = require('../../knexfile');
 const knex        = require('knex')(knexConfig[ENV]);
 const knexLogger  = require('knex-logger');
@@ -70,4 +54,3 @@ exports.seed = function(knex, Promise) {
         });
     });
 };
->>>>>>> upstream/master
