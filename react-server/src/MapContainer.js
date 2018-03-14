2ec7f48c39bb1153ec90144ed3e45515c51fc532
Emily <emily.s.gao@gmail.com>
2018-03-14 13:08:05 -0600

    create new const googleMaps

--- react-server/src/MapContainer.js
+++ react-server/src/MapContainer.js
@@ -1,71 +1,62 @@
-<<<<<<< HEAD
-import { withGoogleMap, GoogleMap, Marker, Circle } from "react-google-maps"
-import { compose, lifecycle } from "recompose";
 import React, { Component } from 'react';
-import { InfoBox } from 'react-google-maps/lib/components/addons/InfoBox';
-=======
-import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-// import React, { Component } from 'react';
-import React from 'react';
-// import { InfoBox } from 'react-google-maps/lib/components/addons/InfoBox';
->>>>>>> c866f5672eed2e095395508f1f70f0d3f5511165
+import bus from './images/bus2.png';
 
 class MapContainer extends Component {
   
   componentDidMount() {
+    const googleMaps = window.google.maps;
 
-    let map = new window.google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
+    const map = new googleMaps.Map(document.getElementById('map'), {
+      zoom: 15,
       center: { lat: this.props.search.latitude, lng: this.props.search.longitude }
     });
   
-    let marker = new window.google.maps.Marker({
+    const marker = new googleMaps.Marker({
       position: { lat: this.props.search.latitude, lng: this.props.search.longitude },
       map: map,
     });
 
-    // let transitLayer = new window.google.maps.TransitLayer();
+    // const transitLayer = new googleMaps.TransitLayer();
     // transitLayer.setMap(map);
 
-    // let cityCircle = new window.google.maps.Circle({
-    //   strokeColor: '#FF0000',
-    //   strokeOpacity: 0.8,
-    //   strokeWeight: 2,
-    //   fillColor: '#FF0000',
-    //   fillOpacity: 0.35,
-    //   map: map,
-    //   radius: this.props.search.radius? this.props.search.radius : 400
-    // });
+    const cityCircle = new googleMaps.Circle({
+      strokeWeight: 0,
+      fillColor: '#87cefa',
+      fillOpacity: 0.25,
+      map: map,
+      center: { lat: this.props.search.latitude, lng: this.props.search.longitude },
+      radius: this.props.search.radius? this.props.search.radius : 800
+    });
 
-    console.log("I've reached PlacesService");
-    let service = new window.google.maps.places.PlacesService(map);
+    const service = new googleMaps.places.PlacesService(map);
     service.nearbySearch({
       location: { lat: this.props.search.latitude, lng: this.props.search.longitude },
-      radius: 500,
+      radius: this.props.search.radius? this.props.search.radius : 800,
       type: ['transit_station']
     }, callback);
     console.log(this.props.search.latitude, this.props.search.longitude);
 
     function callback(results, status) {
-      // if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+      if (status === googleMaps.places.PlacesServiceStatus.OK) {
         console.log(results);
         for (let i = 0; i < results.length; i++) {
           createMarker(results[i]);
         }
-      // }
+      }
     }
 
     function createMarker(place) {
-      let placeLoc = place.geometry.location;
-      let marker = new window.google.maps.Marker({
+      const placeLoc = place.geometry.location;
+      const marker = new googleMaps.Marker({
         map: map,
+        icon: bus,
         position: place.geometry.location
       });
 
       
-      let infowindow = new window.google.maps.InfoWindow();
-      window.google.maps.event.addListener(marker, 'click', function() {
-        console.log(place);
+      const infowindow = new googleMaps.InfoWindow();
+      googleMaps.event.addListener(marker, 'click', function() {
+        // console.log(place);
         infowindow.setContent(place.name);
         infowindow.open(map, this);
       });
@@ -74,7 +65,7 @@ class MapContainer extends Component {
   }
 
   render() {
-    return (<div id='map' style={{ height: `400px`, width: `100%` }} />);
+    return (<div id='map' style={{ height: `600px`, width: `100%` }} />);
   };
 
 }
