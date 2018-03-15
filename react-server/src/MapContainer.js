114c21ea2eda0516e5287ca77a84690d89d4c226
GrinJessie <lijing@ualberta.ca>
2018-03-15 20:55:57 +0000

    fixed lifecyle issue, plug in with transit feature

--- react-server/src/MapContainer.js
+++ react-server/src/MapContainer.js
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import bus from './images/bus2.png';
 
 class MapContainer extends Component {
-  
+  constructor(props) {
+    super(props);
+    console.log('initializing MapContainer constructor');
+    this.initMapAndMarker = this.initMapAndMarker.bind(this);
+  }
+
   componentDidMount() {
+    console.log('in MapContainer componentDidMount');
+    // const search = this.props.search;
+    this.initMapAndMarker(this.props.search);
+  }
+
+  componentWillReceiveProps(nextProps){
+    console.log('in MapContainer componentWillReceiveProps', nextProps);
+    if (this.props.search !== nextProps.search){
+      // const search = nextProps.search
+      this.initMapAndMarker(nextProps.search);
+    }
+  }
+
+  initMapAndMarker(search){
     const googleMaps = window.google.maps;
 
     const map = new googleMaps.Map(document.getElementById('map'), {
       zoom: 15,
-      center: { lat: this.props.search.latitude, lng: this.props.search.longitude }
+      center: { lat: search.latitude, lng: search.longitude }
     });
-  
+
     const marker = new googleMaps.Marker({
-      position: { lat: this.props.search.latitude, lng: this.props.search.longitude },
+      position: { lat: search.latitude, lng: search.longitude },
       map: map,
     });
 
@@ -24,17 +43,17 @@ class MapContainer extends Component {
       fillColor: '#87cefa',
       fillOpacity: 0.25,
       map: map,
-      center: { lat: this.props.search.latitude, lng: this.props.search.longitude },
-      radius: this.props.search.radius? this.props.search.radius : 800
+      center: { lat: search.latitude, lng: search.longitude },
+      radius: search.radius ? search.radius : 800
     });
 
     const service = new googleMaps.places.PlacesService(map);
     service.nearbySearch({
-      location: { lat: this.props.search.latitude, lng: this.props.search.longitude },
-      radius: this.props.search.radius? this.props.search.radius : 800,
+      location: { lat: search.latitude, lng: search.longitude },
+      radius: search.radius ? search.radius : 800,
       type: ['transit_station']
     }, callback);
-    console.log(this.props.search.latitude, this.props.search.longitude);
+    console.log(search.latitude, search.longitude);
 
     function callback(results, status) {
       if (status === googleMaps.places.PlacesServiceStatus.OK) {
@@ -53,9 +72,9 @@ class MapContainer extends Component {
         position: place.geometry.location
       });
 
-      
+
       const infowindow = new googleMaps.InfoWindow();
-      googleMaps.event.addListener(marker, 'click', function() {
+      googleMaps.event.addListener(marker, 'click', function () {
         // console.log(place);
         infowindow.setContent(place.name);
         infowindow.open(map, this);
@@ -65,8 +84,10 @@ class MapContainer extends Component {
   }
 
   render() {
+    console.log('--------------------------------');
+    console.log('redering in MapContainer');
     return (<div id='map' style={{ height: `600px`, width: `100%` }} />);
-  };
+  }
 
 }
 
