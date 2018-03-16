441be1a116bc003edf50a2f0e1d3a222f5559d14
Emily <emily.s.gao@gmail.com>
2018-03-16 13:00:34 -0600

    remove unneeded code

--- react-server/src/Score.js
+++ react-server/src/Score.js
@@ -49,13 +49,6 @@ class Score extends Component {
     return (
       <div>
         <Nav form={true} />
-        {/* <div className="jumbotron">
-          <h1 className="display-4">Hello, find-score page!</h1>
-          <hr className="my-4" />
-            <p className="lead">
-              <a className="btn btn-primary btn-lg" role="button">Learn more</a>
-            </p>
-        </div> */}
         <div className="container mt-2">
           <div className="row">
             <div className="col-8 pl-0">
