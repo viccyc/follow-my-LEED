14a1062878d429a6dece425746a6dced7bdd011f
Viccy Grace <viccy.c.grace@gmail.com>
2018-03-16 02:15:13 +0000

    photo placeholder

--- react-server/src/InfoContent.js
+++ react-server/src/InfoContent.js
@@ -1,5 +1,16 @@
-// import moment from 'moment';
 const moment = window.moment;
+const googleMaps = window.google.maps;
+
+// const createPhotoMarker = (building) => {
+//   const photos = building.photos;
+//   if (!photos) {
+//     return;
+//   }
+
+//   const googlePhoto = new googleMaps.Marker({
+//     icon: photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35})
+//   });
+// }
 
 const certLevel = {
   1: 'LEED Certified',
@@ -9,9 +20,9 @@ const certLevel = {
 };
 
 const createInfoHTML = (item) => {
-  // console.log("item: ", item);
   return (
     `<div>
+      <img src="https://thebigfatnoodle.files.wordpress.com/2011/08/image-from-originalbuzz-info.jpg"height=200 width=200></img>
       <p> ${item.name}. Project # ${item.number} </p>
       <p> Address: ${item.address} </p>
       <p> ${certLevel[item.certification_level_id]}. Date certified: ${moment(item.cert_date).format("MMM DD YYYY")} </p>
