10a07e5252d1774fb43a6ebe8378f7bf1255c556
Emily <emily.s.gao@gmail.com>
2018-03-18 18:03:29 -0600

    update lifecycle methods

--- react-server/src/Projects.js
+++ react-server/src/Projects.js
@@ -1,58 +1,97 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-import Nav from './Nav';
-import ProjectsMapContainer from './ProjectsMapContainer';
 
+import InfoContent from './InfoContent';
+import Home from './Home';
 
-class Projects extends Component {
+import certified from './images/certified.png';
+import silver from './images/silver.png';
+import gold from './images/gold.png';
+import platinum from './images/platinum.png';
+
+export default class Projects extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      address: null,
-      longitude: null,
-      latitude: null,
-      radius: '800'
-    }
-    this.updateStateForData = this.updateStateForData.bind(this);
+    this.initMapAndMarker = this.initMapAndMarker.bind(this);
   }
 
-  componentWillReceiveProps(newProps) {
-    if (this.props !== newProps) {
-      this.updateStateForData(newProps.location.state.data);
-    }
+  componentDidMount() {
+    console.log('componentDidMount');
+    if (this.props.address) {
+      this.initMapAndMarker(this.props.address);
+    };
   }
 
+  componentDidUpdate() {
+    console.log('did update');
+    if (this.props.address) {
+      this.initMapAndMarker(this.props.address);
+    };
+  }
   
-  componentWillMount() {
-    //TODO: fetch backend with location data
-    //need to provide location and radius
-    //with res, use other apis to display and update the map & popups
+  initMapAndMarker(address) {
+    console.log('initMapAndMarker');
+    const googleMaps = window.google.maps;
+    const location = { lat: address.lat, lng: address.lng };
+    
+    const map = new googleMaps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: location
+    });
+    
+    const marker = new googleMaps.Marker({
+      position: location,
+      map: map,
+    });
 
-    //TODO: test google map&popups api
-    // //create map using this.state.location
-    this.updateStateForData(this.props.location.state.data);
+    const circle = new googleMaps.Circle({
+      strokeWeight: 0,
+      fillColor: '#87cefa',
+      fillOpacity: 0.25,
+      map: map,
+      center: location,
+      radius: 800
+    });
+    
+    const icons = {
+      1: certified,
+      2: silver,
+      3: gold, 
+      4: platinum
+    };
 
+    axios.get('/api/projects')
+    .then(res => { 
+      // Create markers.
+      res.data.result.forEach(function(item) {
+        // console.log("creating marker, item: ", item);
+        const marker = new googleMaps.Marker({
+          position: { lat: item.lat, lng: item.lng },
+          // icon: item.level,
+          icon: icons[item.certification_level_id],
+          map: map
+        });
+  
+        const infowindow = new googleMaps.InfoWindow();
+        googleMaps.event.addListener(marker, 'click', function() {
+          // infowindow.setContent(item.name + ',  ' + certLevel[item.certification_level_id]);
+          // infowindow.setContent(InfoContent);
+          // const content = "<html><head><h1><infoContnet/h1></head><body><div><p> Hello InfoContent! </p></div></body></html>";
+          const createdHTML = InfoContent(item);
+          infowindow.setContent(createdHTML);
+          infowindow.open(map, this);
+        });
+      });
+    });
 
-  }
-
-  updateStateForData(data) {
-    const {location, radius} = data;
-    this.setState({
-      address: location.address,
-      longitude: location.longitude,
-      latitude: location.latitude,
-      radius
-    })
   }
 
   render() {
-    return (
-      <div>
-        <Nav form={true} />
-        <ProjectsMapContainer search={this.state}/>
-      </div>
-    );
-  }
+    if (this.props.address) {
+      return (<div id='map' style={{ height: `600px`, width: `100%` }} />);
+    } else {
+      return (<Home location={this.props.location} autocomplete={this.props.autocomplete} handleSearch={this.props.handleSearch} />);
+    }
+  };
 }
-
-export default Projects;
