35b1b2a1005613ee46ae4da6bfee9b08ebd1e50a
Emily <emily.s.gao@gmail.com>
2018-03-19 14:14:04 -0600

    add column to toggle show markers

--- react-server/src/ScoreTable.js
+++ react-server/src/ScoreTable.js
@@ -6,35 +6,47 @@ class ScoreTable extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e) {
-    this.props.handleTableClick(e.currentTarget.value);
+  handleClick(event) {
+    this.props.handleScoreTableClick(event.currentTarget.value);
   }
 
   render() {
+    const toggleButton = (value) => {
+      if (this.props.criteriaClicked.includes(value)) {
+        return (<button type="button" className="btn btn-light" onClick={this.handleClick} value={value} >Hide</button>)
+      } else {
+        return (<button type="button" className="btn btn-light" onClick={this.handleClick} value={value} >Hide</button>)
+      }
+    }
     return (
       <table className="table table-bordered">
         <thead>
           <tr>
             <th scope="col">Criterion</th>
             <th scope="col">Score</th>
+            <th scope="col">Show/Hide Markers</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td><button type="button" className="btn btn-light" onClick={this.handleClick} value="street_network">Street Network</button></td>
+            <td>Street Network</td>
             <td>...</td>
+            <td>{toggleButton('street_network')}</td>
           </tr>
           <tr>
-            <td><button type="button" className="btn btn-light" onClick={this.handleClick} value="community_resources">Community Resources</button></td>
+            <td>Community Resources</td>
             <td>...</td>
+            <td>{toggleButton('community_resources')}</td>
           </tr>
           <tr>
-            <td><button type="button" className="btn btn-light" onClick={this.handleClick} value="access_to_transit">Access to Transit</button></td>
+            <td>Access to Transit</td>
             <td>...</td>
+            <td>{toggleButton('access_to_transit')}</td>
           </tr>
           <tr>
             <td>Total</td>
             <td>...</td>
+            <td></td>
           </tr>
         </tbody>
       </table>
