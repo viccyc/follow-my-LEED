0e02552bce8037665ccf7f85fd1929375b083e3e
Emily <emily.s.gao@gmail.com>
2018-03-19 19:19:50 -0600

    always render Home when no address in state

--- react-server/src/App.js
+++ react-server/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch, Link, withRouter } from 'react-router-dom';
 
-import MenuItem from './MenuItem';
 import NavForm from './NavForm';
 import Home from './Home';
 import Score from './Score';
 import Projects from './Projects';
 import NoMatch from './NoMatch';
+
+import './Nav.css';
+import './NavForm.css';
 
 
 class App extends Component {
@@ -16,12 +18,18 @@ class App extends Component {
     this.state = {
       address: null,
       redirect: false,
-      services: {},
-      area: null,
-      criteriaClicked: []
+      // services: {},
+      // scoreTable: {
+      //   'street_network': null,
+      //   'community_resources': null,
+      //   'access_to_transit': null
+      // },
+      // area: null,
+      // criteriaClicked: []
     };
     this.handleSearch = this.handleSearch.bind(this);
-    this.handleScoreTableClick = this.handleScoreTableClick.bind(this);
+    // this.handleScoreTableClick = this.handleScoreTableClick.bind(this);
+    // this.setMapData = this.setMapData.bind(this);
   }
             
   handleSearch(address, pathname) {
@@ -37,46 +45,72 @@ class App extends Component {
     }
   }
 
-  handleScoreTableClick(value) {
-    let criteriaClicked = this.state.criteriaClicked;
-    if (criteriaClicked.includes(value)) {
-      criteriaClicked = criteriaClicked.filter(criterion => criterion !== value);
-    } else {
-      criteriaClicked.push(value);
-    }
-    this.setState({ criteriaClicked: criteriaClicked });
-  }
+  // handleScoreTableClick(value) {
+  //   let criteriaClicked = this.state.criteriaClicked;
+  //   if (criteriaClicked.includes(value)) {
+  //     criteriaClicked = criteriaClicked.filter(criterion => criterion !== value);
+  //   } else {
+  //     criteriaClicked.push(value);
+  //   }
+  //   this.setState({ criteriaClicked: criteriaClicked });
+  // }
+
+  // setMapData(newState) {
+  //   this.setState(newState);
+  // }
+
+  // setScoreTableData(data) {
+  //   const oldScoreTable = this.state.scoreTable;
+  //   // const keys = Object.keys(oldScoreTable);
+  //   for (let keys in oldScoreTable){
+  //     if (keys === 
+  //   }
+  //   this.state.scoreTable[]
+  // }
 
   render() {
-    const NavFormComponent = () => {
-      if (this.state.address) {
-        return (<NavForm address={this.state.address} handleSearch={this.handleSearch} />);
-      }
-    };
-
     if (this.state.redirect) {
       return (
         <div>
           <nav id="navbar" className="navbar navbar">
-            <Link to="/find_score" style={{ textDecoration: 'none' }}><MenuItem title='CALCULATE SCORE'/></Link>
-            <Link to="/find_score" style={{ textDecoration: 'none' }}><MenuItem title='SHOW PROJECTS'/></Link>
-            {NavFormComponent()}
+            <div>
+              <Link to="/find_score" id="navbar-link1" className="nav-link" style={{ textDecoration: "none" }}>CALCULATE SCORE</Link>
+              <Link to="/projects" id="navbar-link2" className="nav-link" style={{ textDecoration: "none" }}>SHOW PROJECTS</Link>
+            </div>
+            <NavForm address={this.state.address} handleSearch={this.handleSearch} />
           </nav>
-          <Score state={this.state} handleScoreTableClick={this.handleScoreTableClick} />
+          <Score state={this.state} address={this.state.address} />
+        </div>
+      )
+    } else if (!this.state.address) {
+      return (
+        <div>
+          <nav id="navbar" className="navbar navbar">
+            <div>
+              <Link to="/find_score" id="navbar-link1" className="nav-link" style={{ textDecoration: "none" }}>CALCULATE SCORE</Link>
+              <Link to="/projects" id="navbar-link2" className="nav-link" style={{ textDecoration: "none" }}>SHOW PROJECTS</Link>
+            </div>
+          </nav>
+          <div>
+              <Route path='/' render={(props) => <Home {...props} handleSearch={ this.handleSearch } />} />
+          </div>
         </div>
       )
     } else {
       return (
         <div>
           <nav id="navbar" className="navbar navbar">
-            <MenuItem />
-            {NavFormComponent()}
+            <div>
+              <Link to="/find_score" id="navbar-link1" className="nav-link" style={{ textDecoration: "none" }}>CALCULATE SCORE</Link>
+              <Link to="/projects" id="navbar-link2" className="nav-link" style={{ textDecoration: "none" }}>SHOW PROJECTS</Link>
+            </div>
+            <NavForm address={this.state.address} handleSearch={this.handleSearch} />
           </nav>
           <div>
             <Switch>
               <Route exact path='/' render={(props) => <Home {...props} handleSearch={ this.handleSearch } />} />
-              <Route path='/find_score' render={(props) => <Score {...props} state={this.state} handleSearch={ this.handleSearch } handleScoreTableClick={this.handleScoreTableClick} location={this.props.location} />} />
-              <Route path='/projects' render={(props) => <Projects {...props} state={this.state} handleSearch={ this.handleSearch } location={this.props.location} />} />
+              <Route path='/find_score' render={(props) => <Score {...props} state={this.state}  address={this.state.address} handleSearch={ this.handleSearch } location={this.props.location} />} />
+              <Route path='/projects' render={(props) => <Projects {...props} state={this.state} address={this.state.address} handleSearch={ this.handleSearch } location={this.props.location} />} />
               <Route component={NoMatch}/>
             </Switch>
           </div>
