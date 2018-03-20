a533db3b8a01677370a57898b556402a460f66d0
Emily <emily.s.gao@gmail.com>
2018-03-20 16:43:12 -0600

    pass calculate polygon area state to ScoreTable

--- react-server/src/Score.js
+++ react-server/src/Score.js
@@ -4,8 +4,16 @@ import React, { Component } from 'react';
 import Home from './Home';
 import ScoreTable from './ScoreTable';
 
+import communityresourcesImg from './images/communityresources.png';
 import crossroads from './images/crossroads.png';
 import info from './images/info.png';
+import intersectionsImg from './images/intersections.png';
+// import m1 from './images/m1.png';
+// import m2 from './images/m2.png';
+// import m3 from './images/m3.png';
+// import m4 from './images/m4.png';
+// import m5 from './images/m5.png';
+import transitstopsImg from './images/transitstops3.png';
 
 export default class Score extends Component {
 
@@ -210,7 +218,12 @@ export default class Score extends Component {
     googleMaps.event.addListener(drawingManager, 'polygoncomplete', (polygon) => {
       const area = googleMaps.geometry.spherical.computeArea(polygon.getPath());
       console.log('polygon complete, area is ', Math.ceil(area), 'square meters');
-      this.setState({ area: Math.ceil(area) });
+      if (this.state.area) {
+        const newArea = this.state.area + Math.ceil(area);
+        this.setState({ area: newArea });
+      } else {
+        this.setState({ area: Math.ceil(area) });
+      }
     });
 
     let markersList = [];
@@ -219,9 +232,6 @@ export default class Score extends Component {
       { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' }
     );
     const addNewMarker = MarkerClusterer.prototype.addMarker.bind(markerCluster);
-    const setMarkerMap = MarkerClusterer.prototype.setMap.bind(markerCluster);
-    console.log('markerCluster ', markerCluster);
-    console.log('setMarkerMap ', setMarkerMap);
     const service = new googleMaps.places.PlacesService(map);
 
     const showService = (type, label) => {
@@ -247,7 +257,7 @@ export default class Score extends Component {
 
     const createMarker = (place)=>{
       const marker = new googleMaps.Marker({
-        icon: info,
+        icon: communityresourcesImg,
         position: place.geometry.location
       });
       const infowindow = new googleMaps.InfoWindow();
@@ -255,8 +265,7 @@ export default class Score extends Component {
         infowindow.setContent(place.name);
         infowindow.open(map, this);
       });
-      addNewMarker(marker);
-
+      addNewMarker(marker, false);
     };
 
     const filterDistance = (place)=>{
@@ -320,11 +329,9 @@ export default class Score extends Component {
       };
       const callback = (results, status) => {
         if (status !== googleMaps.places.PlacesServiceStatus.OK) return;
-        // countService(services, label, results);
         results.forEach((place) => {
           const marker = new googleMaps.Marker({
-            // map: map,
-            icon: crossroads,
+            icon: transitstopsImg,
             position: place.geometry.location
           });
           const infowindow = new googleMaps.InfoWindow();
@@ -357,25 +364,25 @@ export default class Score extends Component {
     
     // if (criteriaClicked.includes('community_resources')) {
       showService(['supermarket'], 'Supermarket');
-    //   showService(['department_store', 'clothing_store'], 'Clothing store/department store selling clothes');
-      showService(['convenience_store'], 'Convenience Store');
-    //   showService(['hardware_store'], 'Hardware Store');
-    //   showService(['pharmacy'], 'Pharmacy');
-    //   showService(['bank'], 'Bank');
-    //   showService(['gym'], 'Gym, health club, exercise studio');
-    //   showService(['hair_care'], 'Hair care');
-    //   showService(['laundry'], 'Laundry/dry cleaner');
-    //   showService(['bar', 'cafe', 'restaurant'], 'Restaurant/café/diner');
-    //   showService(['art_gallery', 'museum'], 'Cultural arts facility');
-    //   showService(['school'], 'Education facility');
-    //   showService(['bowling_alley', 'movie_theater'], 'Family entertainment venue');
-    //   showService(['local_government_office', 'city_hall'], 'Government office serving public on-site');
-    //   showService(['hospital', 'physiotherapist', 'dentist', 'doctor',], 'Medical clinic/office');
-    //   showService(['church'], 'Place of worship');
-    //   showService(['police', 'fire_station'], 'Police or fire station');
-    //   showService(['post_office'], 'Post office');
-    //   showService(['library'], 'Public library');
-    //   showService(['park'], 'Public park');
+      // showService(['department_store', 'clothing_store'], 'Clothing store/department store selling clothes');
+      // showService(['convenience_store'], 'Convenience Store');
+      // showService(['hardware_store'], 'Hardware Store');
+      // showService(['pharmacy'], 'Pharmacy');
+      // showService(['bank'], 'Bank');
+      // showService(['gym'], 'Gym, health club, exercise studio');
+      // showService(['hair_care'], 'Hair care');
+      // showService(['laundry'], 'Laundry/dry cleaner');
+      // showService(['bar', 'cafe', 'restaurant'], 'Restaurant/café/diner');
+      // showService(['art_gallery', 'museum'], 'Cultural arts facility');
+      // showService(['school'], 'Education facility');
+      // showService(['bowling_alley', 'movie_theater'], 'Family entertainment venue');
+      // showService(['local_government_office', 'city_hall'], 'Government office serving public on-site');
+      // showService(['hospital', 'physiotherapist', 'dentist', 'doctor',], 'Medical clinic/office');
+      // showService(['church'], 'Place of worship');
+      // showService(['police', 'fire_station'], 'Police or fire station');
+      // showService(['post_office'], 'Post office');
+      // showService(['library'], 'Public library');
+      // showService(['park'], 'Public park');
     // }
 
     // get all ways around a certain address
@@ -446,7 +453,7 @@ export default class Score extends Component {
               // console.log(typeof intersection.lat);
               const intersectionMarker = new googleMaps.Marker({
                 // map: map,
-                icon: crossroads,
+                icon: intersectionsImg,
                 position: { lat: intersection.lat, lng: intersection.lon }
               });
               intersectionMarkers.push(intersectionMarker);
@@ -466,17 +473,10 @@ export default class Score extends Component {
             });
             break;
           case 'community_resources':
-            // console.log('in showMarkers ', markersList);
-            console.log('in showMarkers markerCluster.getMarkers()', markerCluster.getMarkers());
             // console.log('in showMarkers markerCluster', markerCluster);
-            // markerCluster.setMap(map);
-            // markersList.forEach((marker) => {
-            //   marker.setMap(map);
-            // });
-            // markerCluster.getMarkers().setVisible(true);
-            // markerCluster.repaint();
-            
-            
+            markerCluster.getMarkers().forEach((marker) => {
+              marker.setMap(map);
+            });           
             break;
           case 'transit_stops':
             transitStopMarkers.forEach((marker) => {
@@ -495,14 +495,10 @@ export default class Score extends Component {
             });
             break;
           case 'community_resources':
-            // console.log('in showMarkers markersList', markersList);
-            console.log('in showMarkers markerCluster.getMarkers()', markerCluster.getMarkers());
-            // console.log('in showMarkers markerCluster', markerCluster);
-            // markerCluster.setMap(null);
-            // markersList.forEach((marker) => {
-            //   marker.setMap(null);
-            // });
-            // markerCluster.getMarkers().setVisible(false);
+            // console.log('in showMarkers markerCluster', markerCluster);            
+            markerCluster.getMarkers().forEach((marker) => {
+              marker.setMap(null);
+            });
             break;
           case 'transit_stops':
             transitStopMarkers.forEach((marker) => {
@@ -519,11 +515,11 @@ export default class Score extends Component {
     return (
       <div className="container mt-2">
         <div className="row">
-          <div className="col-8 pl-0">
-            <div id='map' style={{ height: `600px`, width: `100%` }} />
+          <div className="col-lg-8 col-md-12 pl-0">
+            <div id='map' style={{ height: `88vh`, width: `100%` }} />
           </div>
-          <div id="tableDiv" className="col-4 pr-0">
-            <ScoreTable criteriaClicked={this.state.criteriaClicked} handleClick={this.handleClick} streetNetwork={this.state.streetNetwork} communityResources={this.state.communityResources} transitStops={this.state.transitStops} showMarkers={this.state.showMarkers} hideMarkers={this.state.hideMarkers} />
+          <div id="tableDiv" className="col-lg-4 col-md-12 pr-0">
+            <ScoreTable area={this.state.area} criteriaClicked={this.state.criteriaClicked} handleClick={this.handleClick} streetNetwork={this.state.streetNetwork} communityResources={this.state.communityResources} transitStops={this.state.transitStops} showMarkers={this.state.showMarkers} hideMarkers={this.state.hideMarkers} />
           </div>
         </div>
       </div>
