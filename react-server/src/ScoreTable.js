bcfcaab193ec8e431348f9a0e99a9d23aed6e363
Emily <emily.s.gao@gmail.com>
2018-03-20 10:00:56 -0600

    add handle click event listeners

--- react-server/src/ScoreTable.js
+++ react-server/src/ScoreTable.js
@@ -3,22 +3,57 @@ import React, { Component } from 'react';
 class ScoreTable extends Component {
   constructor(props) {
     super(props);
-    // this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      'street_network': false,
+      'community_resources': false,
+      'transit_stops': false
+    }
+    this.handleHideClick = this.handleHideClick.bind(this);
+    this.handleShowClick = this.handleShowClick.bind(this);
   }
 
-  // handleClick(event) {
-  //   this.props.handleClick(event.currentTarget.value);
-  // }
+  handleHideClick(event) {
+    this.props.hideMarkers(event.currentTarget.value);
+    switch (event.currentTarget.value) {
+      case 'street_network':
+        this.setState({ 'street_network': false });
+        break;
+      case 'community_resources':
+        this.setState({ 'community_resources': false });
+        break;
+      case 'transit_stops':
+        this.setState({ 'transit_stops': false });
+        break;
+      default: break;
+    };
+  }
+
+  handleShowClick(event) {
+    this.props.showMarkers(event.currentTarget.value);
+    switch (event.currentTarget.value) {
+      case 'street_network':
+        this.setState({ 'street_network': true });
+        break;
+      case 'community_resources':
+        this.setState({ 'community_resources': true });
+        break;
+      case 'transit_stops':
+        this.setState({ 'transit_stops': true });
+        break;
+      default: break;
+    };
+    // this.props.showMarkers(event.currentTarget.value);
+  }
 
   render() {
-    const transitStops = this.props.transitStops;
-    // const toggleButton = (value) => {
-    //   if (criteriaClicked && criteriaClicked.includes(value)) {
-    //     return (<button type="button" className="btn btn-light" onClick={this.props.showMarkers(value)} >Hide</button>)
-    //   } else {
-    //     return (<button type="button" className="btn btn-light" onClick={this.props.clearMarkers(value)} >Show</button>)
-    //   }
-    // }
+    // const transitStops = this.props.transitStops;
+    const toggleButton = (value) => {
+      if (this.state[value]) {
+        return (<button type="button" className="btn btn-light" onClick={this.handleHideClick} value={value} >Hide</button>)
+      } else {
+        return (<button type="button" className="btn btn-light" onClick={this.handleShowClick} value={value} >Show</button>)
+      }
+    }
     return (
       <table id="scoreTable" className="table table-bordered">
         <thead>
@@ -32,7 +67,7 @@ class ScoreTable extends Component {
           <tr>
             <td>Street Network</td>
             <td>{this.props.streetNetwork}</td>
-            {/* <td>{toggleButton('street_network')}</td> */}
+            <td>{toggleButton('street_network')}</td>
           </tr>
           <tr>
             <td>Community Resources</td>
@@ -41,7 +76,7 @@ class ScoreTable extends Component {
           </tr>
           <tr>
             <td>Transit Stops</td>
-            <td>{transitStops}</td>
+            <td>{this.props.transitStops}</td>
             {/* <td>{toggleButton('transit_stops')}</td> */}
           </tr>
           <tr>
