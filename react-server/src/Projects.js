5fb630260834f73bcf8f5d17ea6ff4708d3c8f53
Viccy Grace <viccy.c.grace@gmail.com>
2018-03-20 02:20:18 +0000

    adding legend to projects map

--- react-server/src/Projects.js
+++ react-server/src/Projects.js
@@ -88,6 +88,35 @@ export default class Projects extends Component {
   }
 
   render() {
-    return (<div id='map' style={{ height: `600px`, width: `100%` }} />);
+    // return (<div id='map' style={{ height: `600px`, width: `100%` }} />);
+    return (  
+      <div className="container mt-2">
+        <div className="row">
+          <div className="col-10 pl-0">
+            <div id='map' style={{ height: `600px`, width: `100%` }} />
+          </div>
+          <div id="tableDiv" className="col-2 pr-0">
+          <table id="scoreTable">
+              <tr>
+                <td><img src={platinum} /></td>
+               <td>LEED Platinum</td>
+              </tr>
+              <tr>
+                <td>gold</td>
+                <td>LEED Gold</td>
+              </tr>
+              <tr>
+                <td>silver</td>
+                <td>LEED Silver</td>
+              </tr>
+              <tr>
+                <td>certified</td>
+                <td>LEED Certified</td>
+              </tr>
+            </table>
+         </div>
+        </div>
+      </div>
+   )
   };
 }
